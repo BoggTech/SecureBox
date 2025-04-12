5a951d15b62b69f59478a8a9bf5d702b4fa36ed4
BoggTech <113841718+BoggTech@users.noreply.github.com>
2025-04-12 23:04:34 +0100

    Added basic Box integration

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import os.path
 import tkinter as tk
 import logging
 from modules.login import LoginRegister
+from modules.box import BoxConnection
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,6 +16,12 @@ def login_callback(username, private_key):
     # remove login stuff
     for widget in window.winfo_children():
         widget.destroy()
+
+    with open(os.path.join("auth", "box_token"), "r") as f:
+        auth = f.read()
+        box_connection = BoxConnection(auth)
+
+    box_connection.upload_file(os.path.join("files", "example2.txt"), "example2.txt")
 
 login = LoginRegister(master=window, on_complete=login_callback)
 login.grid(column=0, row=0, columnspan=2)
--- modules/box.py
+++ modules/box.py
@@ -1,3 +1,38 @@
 from box_sdk_gen import *
 import os
 
+class BoxConnection:
+    """Class for connecting to Box, reading files, uploading files and getting names of files."""
+    def __init__(self, auth_code):
+        self.auth_code = None
+        self.auth = None
+        self.client = None
+        self.update_auth(auth_code)
+
+    def update_auth(self, auth_code):
+        """Connect to box with a given authentication code"""
+        self.auth_code = auth_code
+        self.auth: BoxDeveloperTokenAuth = BoxDeveloperTokenAuth(token=self.auth_code)
+        self.client: BoxClient = BoxClient(auth=self.auth)
+
+    def upload_file(self, path, output_path=None):
+        """Uploads a file located at path to box"""
+        # TODO: chunked
+        if not output_path:
+            output_path = path
+
+        with open(path, 'rb') as file_content_stream:
+            self.client.uploads.upload_file(
+                UploadFileAttributes(name=output_path, parent=UploadFileAttributesParentField(id="0")),
+                file_content_stream
+            )
+
+    def get_all_files(self):
+        """Returns a list of all filenames in the box"""
+        return self.client.folders.get_folder_items('0').entries
+
+
+
+
+
+
